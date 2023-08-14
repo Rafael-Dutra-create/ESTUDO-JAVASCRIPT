82a99220d6098e2317cfbed11c6fedc77160976e
fael-dutra <104550275+Rafael-Dutra-create@users.noreply.github.com>
2023-08-14 17:08:29 -0300

    $

---
+++ estacio/tema 6-  Integração do Php Com Banco de Dados/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$port = 5432;
+$user = "postgres";
+$pass = "190298rafael";
+$name = "escola";
+$dsn = "pgsql:host=$host;port=$port;dbname=$name";
+
+?>
---
+++ estacio/tema 6-  Integração do Php Com Banco de Dados/listagem.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'inclusao.php';
+require_once 'consulta.php';
+
+if(isset($_POST["matricula"])) {
+    $mensagem = incluir($_POST["matricula"],
+        $_POST["nome"], $_POST["entrada"]);
+    echo("<hr/>".$mensagem."<hr/>");
+}
+$alunos = obterAlunos();
+?>
